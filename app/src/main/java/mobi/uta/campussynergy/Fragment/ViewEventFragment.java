6d39b5b05eb80a45c2d91388a0ffcfccf3a5ea01
Zedd Shmais <zdveloper@gmail.com>
2015-02-07 13:32:46 -0600

    Added fragment files

---
+++ app/src/main/java/mobi/uta/campussynergy/Fragment/ViewEventFragment.java
@@ -0,0 +1,33 @@
+package mobi.uta.campussynergy.Fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by zedd on 2/7/15.
+ */
+public class ViewEventFragment extends Fragment {
+
+
+    public ViewEventFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_view_event, container, false);
+    }
+
+
+
+}
---
+++ app/src/main/java/mobi/uta/campussynergy/Fragment/ViewPageFragment.java
@@ -0,0 +1,33 @@
+package mobi.uta.campussynergy.Fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by zedd on 2/7/15.
+ */
+public class ViewPageFragment extends Fragment {
+
+
+    public ViewPageFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_view_page, container, false);
+    }
+
+
+
+}
