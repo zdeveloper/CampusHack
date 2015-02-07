7aa9d159527d69d6a9822b4e761438494143ed89
Zedd Shmais <zdveloper@gmail.com>
2015-02-07 17:22:44 -0600

    double fixxxx

--- app/src/main/java/mobi/uta/campussynergy/Activity/MainActivity.java
+++ app/src/main/java/mobi/uta/campussynergy/Activity/MainActivity.java
@@ -1,11 +1,11 @@
 package mobi.uta.campussynergy.Activity;
 
-<<<<<<< Updated upstream
+
 import android.content.Intent;
-=======
+
 
 import android.app.ListFragment;
->>>>>>> Stashed changes
+
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
