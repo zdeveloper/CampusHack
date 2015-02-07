400f945ec8749bd796feff782b79e43697dae3f6
Jstaud <james@jfstaudenterprises.com>
2015-02-07 13:26:42 -0600

    Hex 4

--- app/src/main/java/mobi/uta/campussynergy/Fragment/LoginLikesFragment.java
+++ app/src/main/java/mobi/uta/campussynergy/Fragment/LoginLikesFragment.java
@@ -2,7 +2,6 @@ package mobi.uta.campussynergy.Fragment;
 
 import android.app.Fragment;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
