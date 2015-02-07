e710cd1836f2c681c36f9184d9906bfb23f731cc
Zedd Shmais <zdveloper@gmail.com>
2015-02-07 16:21:32 -0600

    Working on map view

---
+++ app/src/main/java/mobi/uta/campussynergy/Fragment/MapViewFragment.java
@@ -0,0 +1,54 @@
+package mobi.uta.campussynergy.Fragment;
+
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import mobi.uta.campussynergy.R;
+
+
+public class MapViewFragment extends Fragment implements OnMapReadyCallback {
+
+    GoogleMap map;
+
+    public MapViewFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
+
+        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+
+        return view;
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.map = googleMap;
+
+        googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(0, 0))
+                .title("Marker"));
+
+        googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+
+    }
+}
